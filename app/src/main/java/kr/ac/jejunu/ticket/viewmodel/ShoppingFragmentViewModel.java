90ab8484af486d46b936932c55fc77fd09042fbd
soohyeon13 <rlatngus6663@gmail.com>
2019-10-17 00:13:18 +0900

    카테고리별 상품목록 구현(nested recyclerview, apollo graphql)

---
+++ app/src/main/java/kr/ac/jejunu/ticket/viewmodel/ShoppingFragmentViewModel.java
@@ -0,0 +1,22 @@
+package kr.ac.jejunu.ticket.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import kr.ac.jejunu.ticket.CategoryListQuery;
+import kr.ac.jejunu.ticket.repository.ProductListDataRepository;
+
+public class ShoppingFragmentViewModel extends AndroidViewModel {
+    private final ProductListDataRepository repository;
+    private LiveData<CategoryListQuery.Data> productCategoryList;
+    public ShoppingFragmentViewModel(@NonNull Application application) {
+        super(application);
+        repository = new ProductListDataRepository();
+        this.productCategoryList = repository.getList();
+    }
+
+    public LiveData<CategoryListQuery.Data> getProductCategoryList(){return productCategoryList;}
+}
